f7a8bf789fff79e544434d30a28f0a82fb309f2c
leofab <leofabr3@gmail.com>
2023-02-17 19:21:07 -0300

    first commit

---
+++ src/dio/java/main/No.java
@@ -0,0 +1,37 @@
+package dio.java.main;
+
+public class No {
+	private Object object;
+	private No refNo;
+	
+	public No(Object object, No refNo) {
+		super();
+		this.object = object;
+		this.refNo = null;
+	}
+
+	public Object getObject() {
+		return object;
+	}
+
+	public void setObject(Object object) {
+		this.object = object;
+	}
+
+	public No getRefNo() {
+		return refNo;
+	}
+
+	public void setRefNo(No refNo) {
+		this.refNo = refNo;
+	}
+
+	@Override
+	public String toString() {
+		return "No [object=" + object + "]";
+	}
+	
+	
+	
+	
+}
